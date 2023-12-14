016140855e66da1ef6eb887353ef6cb99a5e5819
lukcastrodev <lucasrockcastro@gmail.com>
2023-12-14 14:53:37 -0300

    aula 27

---
+++ aula27/Carro.java
@@ -0,0 +1,24 @@
+package ctech.cursoJavaLoiane.aula27;
+
+public class Carro {
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+    void exibirAutonomia(){
+        System.out.println("A autonomia do carro é: " + capCombustivel * consumoCombustivel + "km");
+    }
+
+    double obterAutonomia(){
+        return capCombustivel * consumoCombustivel;
+    }
+
+    double calculaCombustivel(double km){
+
+        double qtdCombustivel = km/consumoCombustivel;
+
+        return qtdCombustivel;
+    }
+}
