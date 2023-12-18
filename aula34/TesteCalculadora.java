2ab57f100c668bd7f54bf4bab67dbe3047dd19b1
lukcastrodev <lucasrockcastro@gmail.com>
2023-12-18 12:41:39 -0300

    aula 34

---
+++ aula34/TesteCalculadora.java
@@ -0,0 +1,13 @@
+package ctech.cursoJavaLoiane.aula34;
+
+public class TesteCalculadora {
+
+    public static void main(String[] args) {
+
+//        MinhaCalculadora calc = new MinhaCalculadora();
+//
+//        calc.soma(1, 2);
+
+        MinhaCalculadora.soma(1, 2);
+    }
+}
