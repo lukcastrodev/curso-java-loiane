a479e15880518387ef6c2f09a7dd9bf6c9635860
lukcastrodev <lucasrockcastro@gmail.com>
2023-12-18 16:07:13 -0300

    aula 37

---
+++ aula37/Teste.java
@@ -0,0 +1,18 @@
+package ctech.cursoJavaLoiane.aula37;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        //Herança
+        /*Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Donn");*/
+
+        //Polimorfismo
+        Pessoa aluno = new Aluno();
+    }
+}
