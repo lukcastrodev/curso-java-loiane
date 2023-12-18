ff4dfdf5f075cd2252708e301b7db37afbba6c56
lukcastrodev <lucasrockcastro@gmail.com>
2023-12-18 12:03:21 -0300

    aula 32

---
+++ aula32/Carro.java
@@ -0,0 +1,33 @@
+package ctech.cursoJavaLoiane.aula32;
+
+public class Carro {
+    private String marca;
+    private String modelo;
+    private int numPassageiros;
+    private double capCombustivel;
+    private double consumoCombustivel;
+
+    public String getMarca(){
+        return this.marca;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public String getModelo(){
+        return this.marca;
+    }
+
+    public void setModelo(String marca){
+        this.marca = marca;
+    }
+
+    public int getNumPassageiros(){
+        return this.numPassageiros;
+    }
+
+    public void setNumPassageiros(int numPassageiros){
+        this.numPassageiros = numPassageiros;
+    }
+}
