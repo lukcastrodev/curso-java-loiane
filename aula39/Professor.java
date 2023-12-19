a7ec982e42e77b20575a234f215ee51fbf42427c
lukcastrodev <lucasrockcastro@gmail.com>
2023-12-19 15:31:29 -0300

    aula 39

---
+++ aula39/Professor.java
@@ -0,0 +1,30 @@
+package ctech.cursoJavaLoiane.aula39;
+
+public class Professor extends Pessoa {
+    private double salario;
+    private String nomeCurso;
+
+    public Professor() {
+        super();
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public double calcularSalarioLiquido(){
+        return 0;
+    }
+}
---
+++ aula39/teste/Aluno.java
@@ -0,0 +1,45 @@
+package ctech.cursoJavaLoiane.aula39.teste;
+
+import ctech.cursoJavaLoiane.aula39.Pessoa;
+
+public class Aluno extends Pessoa {
+    private String curso;
+    private double[] notas;
+
+    public void modificarAcesso(){
+        //super.nome = "Teste";
+        super.nomeVisibilidade = "TesteNomeVisibilidade";
+    }
+    public Aluno() {
+        super();
+    }
+
+    public Aluno(String nome, String endereco, String telefone, String curso) {
+        super(nome, endereco, telefone);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia() {
+        return 0;
+    }
+
+    public boolean verificarAprovado() {
+        return true;
+    }
+}
